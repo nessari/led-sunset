54d9247f4734661e527296dedbd34b36b2ac8d0e
Agnes Sari <sariagnes97@gmail.com>
2023-04-26 18:46:36 +0200

    Refactor

--- led_sunset.py
+++ led_sunset.py
@@ -1,29 +1,24 @@
-from datetime import datetime, time, timedelta
+#!/usr/bin/python3
+
+from datetime import datetime, timedelta
+
 import json
 import subprocess
+import sys
 
 import requests
 
-latitude = '47.4979937'
-longitude = '19.0403594'
-
-switch_off_time = time(21, 30)
-print('Switch off time: ', switch_off_time)
+LATITUDE = '47.4979937'
+LONGITUDE = '19.0403594'
 
 five_minutes = timedelta(minutes=5)
-
 now = datetime.now()
-print('Now: ', now)
-print('Current time: ', now.time())
-
 today = datetime.today()
-# print(today)
-# print(today.date())
 
 # TODO: calculate time zone and dst shifts
-# CEST, DST
+# adjust for timezone and summer shifts (CEST, DST)
 
-def format_time(time_string):
+def parse_time(time_string):
     format = '%I:%M:%S %p'
     time = datetime.strptime(time_string, format)
     return time
@@ -32,46 +27,36 @@ def get_data_from_API():
     sunsets = []
     # create dates for the upcoming week
     week = [(today + timedelta(days=x)).strftime('%Y-%m-%d') for x in range (7)]
-    # print('Dates of the week:', week)
     # call sunrise-sunset API
     for date in week:
-        URL = f'https://api.sunrise-sunset.org/json?lat={latitude}&lng={longitude}&date={date}'
-        resp = json.loads(requests.request('GET', URL).text)
+        URL = f'https://api.sunrise-sunset.org/json?lat={LATITUDE}&lng={LONGITUDE}&date={date}'
+        resp = json.loads(requests.get(URL).text)
         sunset = resp['results']['sunset']
         sunsets.append(sunset)
     weekly_sunsets = dict(zip(week, sunsets))
-    # print('Data to json: ', weekly_sunsets)
     return weekly_sunsets
 
-sunsets = get_data_from_API()
-print('Sunsets', sunsets)
+def cache():
+    sunsets = get_data_from_API()
+    print('Sunsets', sunsets)
+    # write data to json file
+    with open('sunsets.json', 'w') as file:
+        json.dump(sunsets, file)
 
-# write data to json file
+def switch_if_sun_sets():
+    # read todays sunset from json
+    with open('sunsets.json') as sunset_data:
+        cached_sunsets = json.load(sunset_data)
 
-with open('sunsets.json', 'w') as file:
-    json.dump(sunsets, file)
+    todays_sunset = parse_time(cached_sunsets[str(today.date())])
+    cest_sunset = todays_sunset + timedelta(hours=2)
 
+    # switch usb on
+    if now >= (cest_sunset - five_minutes) and now <= (cest_sunset + five_minutes):
+        subprocess.run(['uhubctl', '-l', '1-1', '--ports', '2', '-a', '1'])
 
-# TODO: adjust for time zone and summer shifts
-
-# read todays sunset from json
-
-with open('sunsets.json') as sunset_data:
-    cached_sunsets = json.load(sunset_data)
-print('Cached sunsets', cached_sunsets)
-
-todays_sunset = cached_sunsets[str(today.date())]
-print('Todays sunset: ', todays_sunset)
-
-formatted = format_time(todays_sunset)
-print('Sunset formatted: ', formatted)
-
-cest_sunset = formatted + timedelta(hours=2)
-print('CEST sunset: ', cest_sunset)
-
-# switch ubs off and on
-
-if now >= (formatted - five_minutes) and now <= (formatted + five_minutes):
-    print('Switch on!')
-else:
-    print('Switch off!')
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        switch_if_sun_sets()
+    elif len(sys.argv) == 2 and sys.argv[1] == '--cache':
+        cache()
